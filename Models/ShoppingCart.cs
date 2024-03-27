9a6472de6d5f5bcaeacb1d6c51f2ddf4114c457b
Aurel Brondon <aurelbrondon@gmail.com>
2024-03-27 05:59:10 -0400

    updating shopping cart

--- Models/ShoppingCart.cs
+++ Models/ShoppingCart.cs
@@ -1,7 +1,19 @@
-﻿namespace Red_Mango_API.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Red_Mango_API.Models
 {
     public class ShoppingCart
-
     {
+        public int Id { get; set; }
+        public string UserId { get; set; } 
+
+        public string StripePaymentIntentId { get; set; }
+        public string ClientSecret { get; set; }
+
+        public ICollection<CartItem> CartItems { get; set; }
+
+        [NotMapped]
+        public double CartTotal { get; set; }
     }
+
 }
